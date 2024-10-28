e6bb38751737b83025b66b84b6054106d660d2af
Abbhinav Reddie Nomulla <abbhinav.nomulla656@gmail.com>
2024-10-28 10:45:48 -0700

    Second Commit

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
+// import amplifyconfig from './amplifyconfiguration.json';
 
-Amplify.configure(amplifyconfig);
+// Amplify.configure(amplifyconfig);
 import { appConfig } from './app/app.config';
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
